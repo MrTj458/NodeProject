976c240cfad423ffc746dc6a27607fa340003077
Trevor <thod0127@740s-pl-130.local>
2016-02-02 12:38:08 -0700

    Added CTECArray class  and added contents to header file

---
+++ src/Model/CTECArray.cpp
@@ -0,0 +1,22 @@
+/*
+ * CTECArray.cpp
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: thod0127
+ */
+
+#include "CTECArray.h"
+
+template <class Type>
+CTECArray<Type>::CTECArray()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+template <class Type>
+CTECArray<Type>::~CTECArray()
+{
+	// TODO Auto-generated destructor stub
+}
+
