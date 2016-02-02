dbe929ac4bf73b76b425fee6df8aa8384f6d556c
Trevor <thod0127@740s-pl-130.local>
2016-02-02 13:32:14 -0700

    Added contents to the get() method in the CTECArray class

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "CTECArray.h"
+#include <iostream>
+using namespace std;
 
 template <class Type>
 CTECArray<Type>::CTECArray(int size)
@@ -22,17 +24,39 @@ CTECArray<Type>::~CTECArray()
 template <class Type>
 int CTECArray<Type>::getSize()
 {
+	return this->size;
+}
 
+template <class Type>
+Type* CTECArray<Type>::get(int position)
+{
+	//We need to do bounds cheing so we do not crash the program.
+	if(position >= size || position < 0)
+	{
+		//Out of bounds
+		cerr << "position value is out of bounds :(" << endl;
+		return nullptr;
+	}
+	else
+	{
+		//Inbounds
+		ArrayNode<Type> * current = head;
+		for(int spot = 0; spot <= position; spot++)
+		{
+			if(spot != position)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				return current->getValue();
+			}
+		}
+	}
 }
 
 template <class Type>
 void CTECArray<Type>::set(int position, Type value)
-{
-
-}
-
-template <class Type>
-Type CTECArray<Type>::get(int position)
 {
 
 }
