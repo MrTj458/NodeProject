e67f6c3e5aed32d68bf308634b07e6601e10a2e4
Trevor <thod0127@740s-pl-130.local>
2016-02-18 12:11:02 -0700

    Added documentation to the timer

--- src/Model/Timer.cpp
+++ src/Model/Timer.cpp
@@ -10,16 +10,25 @@
 #include <iomanip>
 using namespace std;
 
+/*
+ * Default constructor.
+ */
 Timer::Timer()
 {
 	executionTime = 0;
 }
 
+/*
+ * Default destructor not used at the moment.
+ */
 Timer::~Timer()
 {
 	// TODO Auto-generated destructor stub
 }
 
+/*
+ * Returns the amount of microseconds the timer ran for as a string.
+ */
 void Timer::displayTimerInformation()
 {
 	cout << fixed;
@@ -29,21 +38,33 @@ void Timer::displayTimerInformation()
 	cout << "Which is " << float(executionTime)/CLOCKS_PER_SEC << " seconds." << endl;
 }
 
+/*
+ * Starts the timer.
+ */
 void Timer::startTimer()
 {
 	executionTime = clock();
 }
 
+/*
+ * Stops the timer
+ */
 void Timer::stopTimer()
 {
 	executionTime = clock() - executionTime;
 }
 
+/*
+ * Resets the timer to 0 microseconds
+ */
 void Timer::resetTimer()
 {
 	executionTime = 0;
 }
 
+/*
+ * Returns the timers execution time as a long in microseconds.
+ */
 long Timer::getExecutionTimeInMicroseconds()
 {
 	return executionTime;
